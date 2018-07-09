096f5ae2c9db0a4464b4a66bef801c9f1b42be3e
StefanBotman <stefanbotman@gmail.com>
2018-07-09 15:40:22 +0200

    update

--- lib/release.sh
+++ lib/release.sh
@@ -146,7 +146,7 @@ if [ ! "$STAGE" == "" ]; then
 		log "reverting: +$SOURCE_VERSION^:featureOne / $STAGE"
 		git clone --single-branch -b featureOne https://github.com/StefanBotman/SFDX
 		ls
-		cd featureOne
+		cd SFDX
 		git push "+$SOURCE_VERSION^:featureOne"
 		#git push origin
 	  fi

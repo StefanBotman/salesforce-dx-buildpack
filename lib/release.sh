3d933312ac59be3dc1c210c2f34b796d32d15c52
StefanBotman <stefanbotman@gmail.com>
2018-07-11 16:47:39 +0200

    update

--- lib/release.sh
+++ lib/release.sh
@@ -145,7 +145,7 @@ if [ ! "$STAGE" == "" ]; then
 		log "Failed deploy - rollback"
 		log "reverting: +$SOURCE_VERSION^:featureOne / $STAGE"
 		log "installing cucumber:"
-		npm install --save-dev cucumber
+		/etc/npm install --save-dev cucumber
 		log "finished installing cucumber"
 		git clone --single-branch -b featureOne https://github.com/StefanBotman/SFDX
 		ls

553d5742ae750e729a1d031c3ba8f33d7cb9ed43
StefanBotman <stefanbotman@gmail.com>
2018-07-12 12:10:09 +0200

    update

--- lib/release.sh
+++ lib/release.sh
@@ -143,7 +143,28 @@ if [ ! "$STAGE" == "" ]; then
 	  if [ "$deployStatusCode" == "0" ]
 	  then
 		log "Installing cucumber for auto testing"
-		npm install --save-dev cucumber
+		mkdir autoTesting
+		cd autoTesting
+		echo '<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+                 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                 xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+        <modelVersion>4.0.0</modelVersion>
+        <groupId>MySel20Proj</groupId>
+        <artifactId>MySel20Proj</artifactId>
+        <version>1.0</version>
+        <dependencies>
+            <dependency>
+                <groupId>org.seleniumhq.selenium</groupId>
+                <artifactId>selenium-server</artifactId>
+                <version>3.0.1</version>
+            </dependency>
+        </dependencies>
+</project>' > pom.xml
+		ls -ltr
+		log "installing selenium"
+		mvn clean install
+		#npm install --save-dev cucumber
 	  fi
 
     else

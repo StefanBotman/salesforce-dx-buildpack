f5590c9cfa780b6e3030e3030a188a60921b8907
StefanBotman <stefanbotman@gmail.com>
2018-07-12 10:09:14 +0200

    update

--- lib/release.sh
+++ lib/release.sh
@@ -145,7 +145,7 @@ if [ ! "$STAGE" == "" ]; then
 		log "Failed deploy - rollback"
 		log "reverting: +$SOURCE_VERSION^:featureOne / $STAGE"
 		log "installing cucumber:"
-		ls /etc/heroku
+		ls $ENV_DIR
 		log "finished installing cucumber"
 		/etc/heroku/heroku config:get GIT_API
 		#ls /etc
